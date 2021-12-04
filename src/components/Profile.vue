3533419672c2db065825393aabad2ee1d5f7615e
Artem <morti.show@gmail.com>
2021-12-04 09:35:32 +0500

    upadte

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -79,25 +79,14 @@
 			}
 		},
 		methods: {
-			getCookie(name) {
-			    var nameEQ = name + "=";
-			    var ca = document.cookie.split(';');
-			    for(var i=0;i < ca.length;i++) {
-			        var c = ca[i];
-			        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-			        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-			    }
-			    return null;
-			},
-		    // checkAuth() {
-		    //     let status = this.getCookie('auth');
-		    //     if (!status) {
-		    //       this.$router.push({path: 'auth'})
-		    //     }
-		    //  }
+		    checkAuth() {
+		        if (document.cookie == '') {
+		          this.$router.push({path: 'auth'})
+		        }
+		     }
 		},
 	    mounted() {
-	      // this.checkAuth();
+	      this.checkAuth();
 	    },
 		created() {
 			this.$bus.$on('isSummary', data => {
